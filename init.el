ce8270ea8b97f06b04a5204991bde0431c028e8b
JJ Lambie <jj@jjs-macbook-air.home>
2024-03-19 21:26:54 +0000

    initial

---
+++ init.el
@@ -0,0 +1,74 @@
+
+;; Initial Housekeeping Set Up
+
+(setq inhibit-startup-message t)	; Disable the splash screen
+
+(scroll-bar-mode -1) 			; Disable the scroll bar
+(tool-bar-mode -1)			; Disable the toolbar
+(tooltip-mode -1)			; Disable tooltips
+(set-fringe-mode 10)			; Add fringe
+(menu-bar-mode -1)			; Disable menu bar
+
+
+;; Display Settings
+
+(setq visible-bell t)			; Set up the visible bell
+
+(if (display-graphic-p)
+    (progn
+      (load-theme 'deeper-blue))
+  (progn
+    (load-theme 'wombat)))
+
+;; Initialise package sources
+
+(require 'package)
+(require 'diminish)
+
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+      ("org" . "https://orgmode.org/elpa/")
+      ("elpa" . "https://elpa.gnu.org/packages/")))
+
+(package-initialize)
+(unless package-archive-contents
+(package-refresh-contents))
+
+;; Initialize use-package on non-Linux platforms
+
+(unless (package-installed-p 'use-package)
+(package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+;; Packages below
+
+;; command-log-mode - lists input in a separate buffer
+
+(use-package command-log-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(persist exercism diminish ivy command-log-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; ivy - autocomplete module
+
+(use-package ivy
+  :diminish)
+(ivy-mode 1)
+
+;; persist - utility to allow persistence
+
+(use-package persist)
+
+;; exercism - intergrate with exercism.io
+
+(use-package exercism)
